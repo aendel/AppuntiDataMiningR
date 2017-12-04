5db28ae7840f299a0cee3832ef07a905a6b38f7e
aendel <fvattiato@gmail.com>
2017-12-04 17:33:46 +0100

    Grafo g2

--- Lezione4.R
+++ Lezione4.R
@@ -8,3 +8,15 @@ library("igraph")
 
 g1 <- graph( edges=c(1,2, 2,3, 3,1),n=3,directed=F)
 plot(g1)
+g1
+
+""" Output g1
+IGRAPH U--- 3 3 -- #attributi
++ edges:
+[1] 1--2 2--3 1--3 #archi
+"""
+
+#n numero di nodi.
+g2 <- graph( edges=c(1,2, 2,3, 3,1),n=10)
+plot(g2)
+g2
