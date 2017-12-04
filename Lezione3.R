5f773796b7a20570c7cb0969dc00a0bf0da421ab
aendel <fvattiato@gmail.com>
2017-12-04 17:31:01 +0100

    Aggiunta esercizio da svolgere lezione 3

--- Lezione3.R
+++ Lezione3.R
@@ -1,3 +1,5 @@
 #Lezione del professore Pulvirenti su Catene di Markov, algoritmo forward, backward,viterbi e posteriori.
 #LIBRERIA HMM Hidden Markov Model
 install.packages("HMM")
+#E 5 I come stati
+#Provare ad implementare Esone 5 Ione 
